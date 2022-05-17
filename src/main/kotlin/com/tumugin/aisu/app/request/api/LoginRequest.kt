134dde63c00cf0c8dad1bc7017321fa0ddefdbbb
Kazuki Oishi <oishikazuki@gmail.com>
2022-05-18 04:25:38 +0900

    ログイン・ログアウトAPIの修正

--- src/main/kotlin/com/tumugin/aisu/app/request/api/LoginRequest.kt
+++ src/main/kotlin/com/tumugin/aisu/app/request/api/LoginRequest.kt
@@ -5,7 +5,9 @@ import com.tumugin.aisu.app.request.ValidatorPatterns.RFC5322EmailPattern
 import io.konform.validation.Validation
 import io.konform.validation.jsonschema.minLength
 import io.konform.validation.jsonschema.pattern
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class LoginRequest(val email: String, val password: String): BaseRequest<LoginRequest> {
   override val validator = Validation<LoginRequest> {
     LoginRequest::email {
--- src/main/kotlin/com/tumugin/aisu/app/responder/login/LoginForbiddenResponder.kt
+++ src/main/kotlin/com/tumugin/aisu/app/responder/login/LoginForbiddenResponder.kt
@@ -1,7 +1,9 @@
 package com.tumugin.aisu.app.responder.login
 
 import com.tumugin.aisu.app.responder.BaseResponder
+import kotlinx.serialization.Serializable
 
+@Serializable
 data class LoginForbiddenResponder(
   override val status: String = "error",
   val message: String = "email or password is incorrect."
