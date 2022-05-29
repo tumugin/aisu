fea7c763706ef2a5286abc9e9da38519e145864c
Kazuki Oishi <oishikazuki@gmail.com>
2022-05-29 17:54:30 +0900

    コメントを修正

--- src/main/kotlin/com/tumugin/aisu/domain/idol/IdolStatus.kt
+++ src/main/kotlin/com/tumugin/aisu/domain/idol/IdolStatus.kt
@@ -1,5 +1,28 @@
 package com.tumugin.aisu.domain.idol
 
 enum class IdolStatus(s: String) {
-  ACTIVE("active"), NOT_ACTIVE("not_active")
+  /**
+   * 公開状態かつ有効(活動中)
+   */
+  PUBLIC_ACTIVE("public_active"),
+
+  /**
+   * 公開状態かつ有効でない(活動休止、解散など)
+   */
+  PUBLIC_NOT_ACTIVE("public_not_active"),
+
+  /**
+   * 非公開状態(作ったユーザのみが閲覧可能)かつ有効(活動中)
+   */
+  PRIVATE_ACTIVE("private_active"),
+
+  /**
+   * 非公開状態(作ったユーザのみが閲覧可能)かつ有効でない(活動休止、解散など)
+   */
+  PRIVATE_NOT_ACTIVE("private_not_active"),
+
+  /**
+   * 運営削除(論理削除)
+   */
+  OPERATION_DELETED("operation_deleted")
 }
