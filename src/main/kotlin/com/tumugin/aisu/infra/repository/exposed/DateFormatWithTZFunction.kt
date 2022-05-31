da835933a671235383b0962f8d0181a04849cbd0
Kazuki Oishi <oishikazuki@gmail.com>
2022-06-01 04:29:43 +0900

    書き方間違えてた

--- src/main/kotlin/com/tumugin/aisu/infra/repository/exposed/DateFormatWithTZFunction.kt
+++ src/main/kotlin/com/tumugin/aisu/infra/repository/exposed/DateFormatWithTZFunction.kt
@@ -11,6 +11,6 @@ class DateFormatWithTZFunction<T : ExpressionWithColumnType<Instant>>(
   private val exp: T, private val format: String, private val fromTZ: TimeZone, private val toTZ: TimeZone
 ) : org.jetbrains.exposed.sql.Function<String>(CharColumnType()) {
   override fun toQueryBuilder(queryBuilder: QueryBuilder) = queryBuilder {
-    append("CONVERT_TZ(DATE_FORMAT(", exp, ", '$format'), '${fromTZ.id}', '${toTZ.id}')")
+    append("DATE_FORMAT(CONVERT_TZ(", exp, ", '${fromTZ.id}', '${toTZ.id}'), '${format}')")
   }
 }
