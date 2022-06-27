7386bff913c43c700080e75bc3c38ca15e43916e
Kazuki Oishi <oishikazuki@gmail.com>
2022-06-28 05:04:36 +0900

    add test

--- src/test/kotlin/com/tumugin/aisu/testing/usecase/client/cheki/GetChekiMonthIdolCountByUserIdAndIdolTest.kt
+++ src/test/kotlin/com/tumugin/aisu/testing/usecase/client/cheki/GetChekiMonthIdolCountByUserIdAndIdolTest.kt
@@ -1,16 +1,17 @@
 package com.tumugin.aisu.testing.usecase.client.cheki
 
+import com.tumugin.aisu.domain.cheki.*
 import com.tumugin.aisu.domain.group.Group
 import com.tumugin.aisu.domain.idol.Idol
 import com.tumugin.aisu.domain.regulation.Regulation
 import com.tumugin.aisu.domain.user.User
 import com.tumugin.aisu.testing.BaseDatabaseTest
-import com.tumugin.aisu.testing.seeder.GroupSeeder
-import com.tumugin.aisu.testing.seeder.IdolSeeder
-import com.tumugin.aisu.testing.seeder.RegulationSeeder
-import com.tumugin.aisu.testing.seeder.UserSeeder
+import com.tumugin.aisu.testing.seeder.*
 import com.tumugin.aisu.usecase.client.cheki.GetCheki
 import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.Instant
+import kotlinx.datetime.TimeZone
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -33,6 +34,29 @@ class GetChekiMonthIdolCountByUserIdAndIdolTest : BaseDatabaseTest() {
 
   @Test
   fun testGetChekiMonthIdolCountByUserIdAndIdol() = runTest {
-    TODO()
+    ChekiSeeder().seedCheki(
+      user.userId,
+      idol.idolId,
+      regulation.regulationId,
+      chekiShotAt = ChekiShotAt(Instant.parse("2021-12-01T00:00:00+09:00")),
+      chekiQuantity = ChekiQuantity(1)
+    )
+    ChekiSeeder().seedCheki(
+      user.userId,
+      idolTwo.idolId,
+      regulation.regulationId,
+      chekiShotAt = ChekiShotAt(Instant.parse("2022-01-01T00:00:00+09:00")),
+      chekiQuantity = ChekiQuantity(10)
+    )
+
+    val chekiMonthIdolCount =
+      getCheki.getChekiMonthIdolCountByUserIdAndIdol(user.userId, idol.idolId, TimeZone.of("Asia/Tokyo"))
+    Assertions.assertEquals(
+      listOf(
+        ChekiMonthIdolCount(idol, ChekiCount(1), ChekiShotAtMonth(2021, 12, TimeZone.of("Asia/Tokyo"))),
+        ChekiMonthIdolCount(idolTwo, ChekiCount(10), ChekiShotAtMonth(2022, 1, TimeZone.of("Asia/Tokyo")))
+      ),
+      chekiMonthIdolCount
+    )
   }
 }
