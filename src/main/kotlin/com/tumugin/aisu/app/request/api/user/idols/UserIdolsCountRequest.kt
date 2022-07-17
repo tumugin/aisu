d4a4c7821403ab6badf69642b476f500fe252fb7
Kazuki Oishi <oishikazuki@gmail.com>
2022-07-17 16:36:13 +0900

    fix validator

--- src/main/kotlin/com/tumugin/aisu/app/request/api/user/idols/UserIdolsCountRequest.kt
+++ src/main/kotlin/com/tumugin/aisu/app/request/api/user/idols/UserIdolsCountRequest.kt
@@ -2,13 +2,19 @@ package com.tumugin.aisu.app.request.api.user.idols
 
 import com.tumugin.aisu.app.request.BaseRequest
 import io.konform.validation.Validation
+import io.konform.validation.jsonschema.enum
+import io.konform.validation.jsonschema.minLength
+import kotlinx.datetime.TimeZone
 
 class UserIdolsCountRequest(private val baseTimezone: String?) : BaseRequest<UserIdolsCountRequest> {
   override val validator: Validation<UserIdolsCountRequest> = Validation {
     UserIdolsCountRequest::baseTimezone required {
-
+      enum(*TimeZone.availableZoneIds.toTypedArray())
     }
   }
+
+  val castedBaseTimezone: TimeZone
+    get() = TimeZone.of(baseTimezone!!)
 
   init {
     validate(this)
