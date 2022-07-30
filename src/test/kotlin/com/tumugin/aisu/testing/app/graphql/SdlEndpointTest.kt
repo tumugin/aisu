163141a45fe47ee2e586675c104be1764218c2fd
Kazuki Oishi <oishikazuki@gmail.com>
2022-07-31 04:42:53 +0900

    test sdl endpoint

---
+++ src/test/kotlin/com/tumugin/aisu/testing/app/graphql/SdlEndpointTest.kt
@@ -0,0 +1,16 @@
+package com.tumugin.aisu.testing.app.graphql
+
+import com.tumugin.aisu.testing.BaseKtorTest
+import io.ktor.client.request.*
+import io.ktor.http.*
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class SdlEndpointTest : BaseKtorTest() {
+  @Test
+  fun testSdlEndpoint() = testAisuApplication {
+    client.get("/sdl").apply {
+      Assertions.assertEquals(HttpStatusCode.OK, status)
+    }
+  }
+}
