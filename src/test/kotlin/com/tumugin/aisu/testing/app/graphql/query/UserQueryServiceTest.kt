0cbb19179d802576fba81fea58b75ded4854f299
Kazuki Oishi <oishikazuki@gmail.com>
2022-07-30 19:48:06 +0900

    Add graphql testing environment

--- src/test/kotlin/com/tumugin/aisu/testing/app/graphql/query/UserQueryServiceTest.kt
+++ src/test/kotlin/com/tumugin/aisu/testing/app/graphql/query/UserQueryServiceTest.kt
@@ -1,9 +1,19 @@
 package com.tumugin.aisu.testing.app.graphql.query
 
+import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
 import com.tumugin.aisu.testing.BaseKtorTest
+import com.tumugin.aisu.testing.graphql.client.CurrentUser
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.net.URL
 
 class UserQueryServiceTest : BaseKtorTest() {
 
-  fun testGetCurrentUser() = testAisuApplication {
+  @Test
+  fun testGetCurrentUserWithNotLoggedIn() = testAisuApplication {
+    val graphQLClient = GraphQLKtorClient(URL("http://localhost/graphql"), client)
+    val result = graphQLClient.execute(CurrentUser())
+    Assertions.assertNull(result.data?.currentUser)
+    Assertions.assertNull(result.errors)
   }
 }
