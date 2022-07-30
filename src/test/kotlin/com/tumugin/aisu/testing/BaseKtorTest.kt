2097f9d59eb17ba798d171561df15b22d91cca2b
Kazuki Oishi <oishikazuki@gmail.com>
2022-07-30 22:32:15 +0900

    Add test

--- src/test/kotlin/com/tumugin/aisu/testing/BaseKtorTest.kt
+++ src/test/kotlin/com/tumugin/aisu/testing/BaseKtorTest.kt
@@ -1,17 +1,20 @@
 package com.tumugin.aisu.testing
 
+import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
 import com.tumugin.aisu.app.request.api.LoginRequest
 import com.tumugin.aisu.createKtorModule
 import com.tumugin.aisu.domain.app.csrf.CSRFRepository
 import com.tumugin.aisu.domain.user.User
 import com.tumugin.aisu.domain.user.UserRawPassword
 import com.tumugin.aisu.testing.seeder.UserSeeder
+import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import org.koin.core.component.inject
+import java.net.URL
 
 abstract class BaseKtorTest : BaseDatabaseTest() {
   private val csrfRepository by inject<CSRFRepository>()
@@ -41,6 +44,10 @@ abstract class BaseKtorTest : BaseDatabaseTest() {
   suspend fun seedUserAndLoginAndGetCookieValue(builder: ApplicationTestBuilder): LoggedUserAndCookieValue {
     val user = UserSeeder().seedNonDuplicateUser(userRawPassword = UserRawPassword("password"))
     return LoggedUserAndCookieValue(user, loginAndGetCookieValue(builder, user.userEmail!!.value, "password"))
+  }
+
+  fun createGraphQLKtorClient(client: HttpClient): GraphQLKtorClient {
+    return GraphQLKtorClient(URL("http://localhost/graphql"), client)
   }
 }
 
