d1fa9ea9d755417c4ecdc4aee2a08dfd37aa3495
Kazuki Oishi <oishikazuki@gmail.com>
2022-08-23 03:54:54 +0900

    add test

--- src/test/kotlin/com/tumugin/aisu/testing/app/graphql/mutation/idolMutationService/DeleteIdolTest.kt
+++ src/test/kotlin/com/tumugin/aisu/testing/app/graphql/mutation/idolMutationService/DeleteIdolTest.kt
@@ -3,17 +3,14 @@ package com.tumugin.aisu.testing.app.graphql.mutation.idolMutationService
 import com.tumugin.aisu.domain.idol.Idol
 import com.tumugin.aisu.domain.user.User
 import com.tumugin.aisu.testing.BaseKtorTest
-import com.tumugin.aisu.testing.graphql.client.UpdateIdolMutation
-import com.tumugin.aisu.testing.graphql.client.enums.IdolStatus
-import com.tumugin.aisu.testing.graphql.client.inputs.AddOrUpdateIdolParamsInput
+import com.tumugin.aisu.testing.graphql.client.DeleteIdolMutation
 import com.tumugin.aisu.testing.seeder.IdolSeeder
-import com.tumugin.aisu.testing.seeder.UserSeeder
 import io.ktor.client.request.*
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class UpdateIdolTest : BaseKtorTest() {
+class DeleteIdolTest : BaseKtorTest() {
   lateinit var user: User
   lateinit var cookieValue: String
   lateinit var idol: Idol
@@ -27,14 +24,11 @@ class UpdateIdolTest : BaseKtorTest() {
   }
 
   @Test
-  fun testUpdateIdol() = testAisuApplication {
+  fun testDeleteIdol() = testAisuApplication {
     val graphQLClient = createGraphQLKtorClient(client)
     val result = graphQLClient.execute(
-      UpdateIdolMutation(
-        UpdateIdolMutation.Variables(
-          AddOrUpdateIdolParamsInput(
-            "藍井すず", IdolStatus.PUBLIC_ACTIVE
-          ),
+      DeleteIdolMutation(
+        DeleteIdolMutation.Variables(
           idol.idolId.value.toString()
         )
       )
