9609c25bdfea31447a3f4e4b396d04967ca2fccf
Kazuki Oishi <oishikazuki@gmail.com>
2022-09-12 02:31:35 +0900

    add update regulation test

--- src/test/kotlin/com/tumugin/aisu/testing/app/graphql/mutation/regulation/UpdateRegulationTest.kt
+++ src/test/kotlin/com/tumugin/aisu/testing/app/graphql/mutation/regulation/UpdateRegulationTest.kt
@@ -2,17 +2,17 @@ package com.tumugin.aisu.testing.app.graphql.mutation.regulation
 
 import com.tumugin.aisu.domain.user.User
 import com.tumugin.aisu.testing.BaseKtorTest
-import com.tumugin.aisu.testing.graphql.client.AddRegulation
+import com.tumugin.aisu.testing.graphql.client.UpdateRegulation
 import com.tumugin.aisu.testing.graphql.client.enums.RegulationStatus
 import com.tumugin.aisu.testing.graphql.client.inputs.AddOrUpdateRegulationParamsInput
 import com.tumugin.aisu.testing.seeder.GroupSeeder
-import com.tumugin.aisu.testing.seeder.UserSeeder
+import com.tumugin.aisu.testing.seeder.RegulationSeeder
 import io.ktor.client.request.*
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
-class AddRegulationTest : BaseKtorTest() {
+class UpdateRegulationTest : BaseKtorTest() {
   lateinit var user: User
   lateinit var cookieValue: String
 
@@ -24,13 +24,14 @@ class AddRegulationTest : BaseKtorTest() {
   }
 
   @Test
-  fun testAddRegulation() = testAisuApplication {
+  fun testUpdateRegulation() = testAisuApplication {
     val group = GroupSeeder().seedGroup(user.userId)
+    val regulation = RegulationSeeder().seedRegulation(group.groupId, user.userId)
     val graphQLClient = createGraphQLKtorClient(client)
     val result = graphQLClient.execute(
-      AddRegulation(
-        AddRegulation.Variables(
-          AddOrUpdateRegulationParamsInput(
+      UpdateRegulation(
+        UpdateRegulation.Variables(
+          regulation.regulationId.value.toString(), AddOrUpdateRegulationParamsInput(
             group.groupId.value.toString(), "てすと", "2ショチェキ(サインなし)", RegulationStatus.ACTIVE, 2000
           )
         )
@@ -40,6 +41,6 @@ class AddRegulationTest : BaseKtorTest() {
     }
 
     Assertions.assertNull(result.errors)
-    Assertions.assertNotNull(result.data?.regulation?.addRegulation)
+    Assertions.assertNotNull(result.data?.regulation?.updateRegulation)
   }
 }
