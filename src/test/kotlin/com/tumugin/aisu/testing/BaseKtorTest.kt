1211239994999e751c3bc65d1ba47c36c6faf173
Kazuki Oishi <oishikazuki@gmail.com>
2022-09-17 08:07:18 +0900

    テスト時に使うセッション周りを修正した

--- src/test/kotlin/com/tumugin/aisu/testing/BaseKtorTest.kt
+++ src/test/kotlin/com/tumugin/aisu/testing/BaseKtorTest.kt
@@ -1,7 +1,7 @@
 package com.tumugin.aisu.testing
 
 import com.expediagroup.graphql.client.ktor.GraphQLKtorClient
-import com.tumugin.aisu.app.request.api.LoginRequest
+import com.tumugin.aisu.app.plugins.UserAuthSession
 import com.tumugin.aisu.createKtorModule
 import com.tumugin.aisu.domain.app.csrf.CSRFRepository
 import com.tumugin.aisu.domain.user.User
@@ -10,11 +10,14 @@ import com.tumugin.aisu.testing.seeder.UserSeeder
 import io.ktor.client.*
 import io.ktor.client.request.*
 import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import io.ktor.server.sessions.*
 import io.ktor.server.testing.*
-import kotlinx.serialization.encodeToString
-import kotlinx.serialization.json.Json
+import kotlinx.datetime.Clock
 import org.koin.core.component.inject
 import java.net.URL
+import kotlin.time.Duration.Companion.days
 
 abstract class BaseKtorTest : BaseDatabaseTest() {
   private val csrfRepository by inject<CSRFRepository>()
@@ -32,12 +35,19 @@ abstract class BaseKtorTest : BaseDatabaseTest() {
     builder.header("X-CSRF-Token", csrfRepository.generateToken().value)
   }
 
-  suspend fun loginAndGetCookieValue(builder: ApplicationTestBuilder, email: String, password: String): String {
-    builder.client.post("/api/login") {
-      contentType(ContentType.Application.Json)
-      setBody(Json.encodeToString(LoginRequest(email, password)))
-      addCSRFTokenHeader(this)
-    }.apply {
+  suspend fun loginAndGetCookieValue(builder: ApplicationTestBuilder, user: User): String {
+    builder.routing {
+      get("/test-add-session") {
+        call.sessions.set(
+          UserAuthSession(
+            userId = user.userId.value,
+            validThroughTimestamp = Clock.System.now().plus(30.days).toString(),
+            forceLogoutGeneration = user.userForceLogoutGeneration.value
+          )
+        )
+      }
+    }
+    builder.client.get("/test-add-session").apply {
       return headers.getAll("Set-Cookie")!!.map { parseServerSetCookieHeader(it) }
         .map { (it.name).encodeURLParameter() + "=" + (it.value).encodeURLParameter() }.joinToString("; ")
     }
@@ -45,7 +55,7 @@ abstract class BaseKtorTest : BaseDatabaseTest() {
 
   suspend fun seedUserAndLoginAndGetCookieValue(builder: ApplicationTestBuilder): LoggedUserAndCookieValue {
     val user = UserSeeder().seedNonDuplicateUser(userRawPassword = UserRawPassword("password"))
-    return LoggedUserAndCookieValue(user, loginAndGetCookieValue(builder, user.userEmail!!.value, "password"))
+    return LoggedUserAndCookieValue(user, loginAndGetCookieValue(builder, user))
   }
 
   fun createGraphQLKtorClient(client: HttpClient): GraphQLKtorClient {
@@ -53,5 +63,4 @@ abstract class BaseKtorTest : BaseDatabaseTest() {
   }
 }
 
-data class LoggedUserAndCookieValue(val user: User, val cookieValue: String) {
-}
+data class LoggedUserAndCookieValue(val user: User, val cookieValue: String) {}
