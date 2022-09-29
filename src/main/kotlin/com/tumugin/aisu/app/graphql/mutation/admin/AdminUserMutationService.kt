c2dc954590c0c480cef4bac74d0b47d5fe3c0683
Kazuki Oishi <oishikazuki@gmail.com>
2022-09-30 02:18:56 +0900

    add graphql mutation

--- src/main/kotlin/com/tumugin/aisu/app/graphql/mutation/admin/AdminUserMutationService.kt
+++ src/main/kotlin/com/tumugin/aisu/app/graphql/mutation/admin/AdminUserMutationService.kt
@@ -1,16 +1,41 @@
 package com.tumugin.aisu.app.graphql.mutation.admin
 
+import com.expediagroup.graphql.generator.scalars.ID
 import com.expediagroup.graphql.server.operations.Mutation
 import com.tumugin.aisu.app.graphql.params.admin.AddAdminUserParams
+import com.tumugin.aisu.app.graphql.params.admin.UpdateAdminUserParams
+import com.tumugin.aisu.app.graphql.params.admin.UpdateAdminUserPasswordParams
 import com.tumugin.aisu.app.serializer.admin.AdminUserSerializer
+import com.tumugin.aisu.domain.adminUser.AdminUserId
 import com.tumugin.aisu.usecase.admin.adminUser.CreateAdminUser
+import com.tumugin.aisu.usecase.admin.adminUser.UpdateAdminUser
 
 class AdminUserMutationService : Mutation {
   private val createAdminUser = CreateAdminUser()
+  private val updateAdminUser = UpdateAdminUser()
 
   suspend fun addAdminUser(params: AddAdminUserParams): AdminUserSerializer {
     val adminUser = createAdminUser.createAdminUser(
       params.castedAdminUserName, params.castedAdminUserEmail, params.castedAdminUserRawPassword.toHashedPassword()
+    )
+
+    return AdminUserSerializer.from(adminUser)
+  }
+
+  suspend fun updateAdminUser(adminUserId: ID, params: UpdateAdminUserParams): AdminUserSerializer {
+    val adminUser = updateAdminUser.updateAdminUser(
+      AdminUserId(adminUserId.value.toLong()),
+      params.castedAdminUserName,
+      params.castedAdminUserEmail,
+    )
+
+    return AdminUserSerializer.from(adminUser)
+  }
+
+  suspend fun updateAdminUserPassword(adminUserId: ID, params: UpdateAdminUserPasswordParams): AdminUserSerializer {
+    val adminUser = updateAdminUser.updateAdminUserPassword(
+      AdminUserId(adminUserId.value.toLong()),
+      params.castedAdminUserRawPassword,
     )
 
     return AdminUserSerializer.from(adminUser)
