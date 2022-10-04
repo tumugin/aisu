39a873818238fd98a5fdfb84286d8134c98ffa99
Kazuki Oishi <oishikazuki@gmail.com>
2022-10-05 06:02:04 +0900

    add seeder

---
+++ src/test/kotlin/com/tumugin/aisu/testing/seeder/Auth0UserSeeder.kt
@@ -0,0 +1,21 @@
+package com.tumugin.aisu.testing.seeder
+
+import com.tumugin.aisu.domain.auth0.*
+import org.koin.core.component.KoinComponent
+import org.koin.core.component.inject
+import java.util.*
+
+class Auth0UserSeeder : KoinComponent {
+  private val auth0UserRepository by inject<Auth0UserRepository>()
+
+  suspend fun seedAuth0User(
+    auth0UserId: Auth0UserId = Auth0UserId("${UUID.randomUUID()}|test"),
+    auth0UserName: Auth0UserName = Auth0UserName("藍井すず")
+  ): Auth0User {
+    return auth0UserRepository.addAuth0User(
+      Auth0UserInfo(
+        auth0UserId = auth0UserId, auth0UserName = auth0UserName
+      )
+    )
+  }
+}
