9fbd2aa479174f779636b383651183062e522842
Kazuki Oishi <oishikazuki@gmail.com>
2024-01-01 23:29:50 +0900

    Fixed startup logging

--- src/main/kotlin/com/tumugin/aisu/Application.kt
+++ src/main/kotlin/com/tumugin/aisu/Application.kt
@@ -6,18 +6,26 @@ import com.tumugin.aisu.usecase.app.ApplicationBootstrap
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
+import io.ktor.util.logging.*
 import org.koin.core.Koin
 import org.koin.core.context.GlobalContext
+import org.slf4j.LoggerFactory
 
 fun main() {
-  AisuDIModule.start()
-  ApplicationBootstrap().bootstrap()
-  embeddedServer(
-    Netty,
-    port = 8080,
-    host = "0.0.0.0",
-    module = createKtorModule(GlobalContext.get())
-  ).start(wait = true)
+  val logger = LoggerFactory.getLogger("Startup")
+  try {
+    AisuDIModule.start()
+    ApplicationBootstrap().bootstrap()
+    embeddedServer(
+      Netty,
+      port = 8080,
+      host = "0.0.0.0",
+      module = createKtorModule(GlobalContext.get())
+    ).start(wait = true)
+  } catch (e: Exception) {
+    logger.error(e)
+    throw e
+  }
 }
 
 fun createKtorModule(koin: Koin): Application.() -> Unit {
