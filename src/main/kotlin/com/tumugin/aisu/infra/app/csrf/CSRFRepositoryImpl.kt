dbec6d628165de4f86e3a1bf44ce977938f14838
Kazuki Oishi <oishikazuki@gmail.com>
2024-01-26 07:13:33 +0900

    Fix the memory leak bug

--- src/main/kotlin/com/tumugin/aisu/infra/app/csrf/CSRFRepositoryImpl.kt
+++ src/main/kotlin/com/tumugin/aisu/infra/app/csrf/CSRFRepositoryImpl.kt
@@ -3,28 +3,35 @@ package com.tumugin.aisu.infra.app.csrf
 import com.tumugin.aisu.domain.app.csrf.CSRFRepository
 import com.tumugin.aisu.domain.app.csrf.CSRFToken
 import com.tumugin.aisu.domain.app.database.RedisPoolRepository
+import io.lettuce.core.ExperimentalLettuceCoroutinesApi
 import io.lettuce.core.api.StatefulRedisConnection
-import kotlinx.coroutines.future.await
+import io.lettuce.core.api.coroutines.RedisCoroutinesCommands
+import io.lettuce.core.support.BoundedAsyncPool
 import java.util.*
 import kotlin.time.Duration.Companion.hours
 
-class CSRFRepositoryImpl(private val redisPoolRepository: RedisPoolRepository<StatefulRedisConnection<String, String>>) :
+@OptIn(ExperimentalLettuceCoroutinesApi::class)
+class CSRFRepositoryImpl(
+  private val redisPoolRepository: RedisPoolRepository<RedisCoroutinesCommands<String, String>, BoundedAsyncPool<StatefulRedisConnection<String, String>>>
+) :
   CSRFRepository {
   private val keySuffix = "csrf:"
   private val sessionExpireDuration = 12.hours
 
   override suspend fun generateToken(): CSRFToken {
-    val connection = redisPoolRepository.getConnection().async()
     val token = CSRFToken(UUID.randomUUID().toString())
-    connection.set(getKey(token), "").await()
-    connection.expire(getKey(token), sessionExpireDuration.inWholeSeconds).await()
+    redisPoolRepository.borrow { c ->
+      c.set(getKey(token), "")
+      c.expire(getKey(token), sessionExpireDuration.inWholeSeconds)
+    }
 
     return token
   }
 
   override suspend fun validateTokenExists(token: CSRFToken): Boolean {
-    val connection = redisPoolRepository.getConnection().async()
-    return connection.exists(getKey(token)).await() > 0
+    return redisPoolRepository.borrow { c ->
+      c.exists(getKey(token)) == 1.toLong()
+    }
   }
 
   private fun getKey(id: CSRFToken): String {
--- src/main/kotlin/com/tumugin/aisu/usecase/app/Database.kt
+++ src/main/kotlin/com/tumugin/aisu/usecase/app/Database.kt
@@ -2,16 +2,20 @@ package com.tumugin.aisu.usecase.app
 
 import com.tumugin.aisu.domain.app.database.JDBCConnectionRepository
 import com.tumugin.aisu.domain.app.database.RedisPoolRepository
+import io.lettuce.core.ExperimentalLettuceCoroutinesApi
 import io.lettuce.core.api.StatefulRedisConnection
+import io.lettuce.core.api.coroutines.RedisCoroutinesCommands
+import io.lettuce.core.support.BoundedAsyncPool
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
 
+@OptIn(ExperimentalLettuceCoroutinesApi::class)
 class Database : KoinComponent {
   private val jdbcConnectionRepository by inject<JDBCConnectionRepository>()
-  private val redisPool by inject<RedisPoolRepository<StatefulRedisConnection<String, String>>>()
+  private val redisPool by inject<RedisPoolRepository<RedisCoroutinesCommands<String, String>, BoundedAsyncPool<StatefulRedisConnection<String, String>>>>()
 
   suspend fun prepareDatabase() {
     this.jdbcConnectionRepository.prepareORM()
-    this.redisPool.getConnection()
+    this.redisPool.getPoolOrCreate()
   }
 }
